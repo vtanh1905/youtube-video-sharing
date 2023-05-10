32dd32c3c2f49bf89b55d579a0c20b2d43ba35fa
unknown <vtanh1905@gmail.com>
2023-05-10 21:14:05 +0700

    Server: Setup Swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config'
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from 'express'
+import * as swaggerUi from 'swagger-ui-express'
+import fs from 'fs';
+import * as YAML from 'yaml'
 
-const app: Express = express();
-const port = process.env.PORT;
+const swaggerDocument = YAML.parse(fs.readFileSync(`${__dirname}/../swagger/openapi.yaml`, 'utf8'))
+
+const app: Express = express()
+const port = process.env.PORT
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  res.send('Express + TypeScript Server')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
