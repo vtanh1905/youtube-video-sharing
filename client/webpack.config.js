75438fc07a8c0dccbffa7a4ae0e0c293f48a4cfd
unknown <vtanh1905@gmail.com>
2023-05-11 20:47:33 +0700

    Client: First Commit

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "build"),
+    },
+    compress: true,
+    port: 8000,
+  },
+};
