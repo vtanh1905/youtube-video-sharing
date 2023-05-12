29f70172bf4d97ffab4de070b40a132f3d1be4d6
unknown <vtanh1905@gmail.com>
2023-05-12 20:16:04 +0700

    Client: Registry Page UI

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Layout as LayoutAntd, Button, Dropdown } from 'antd'
+import { Layout as LayoutAntd, Button, Space } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined } from '@ant-design/icons'
 import './style.scss'
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
 import Login from '../Login'
 
@@ -12,12 +12,11 @@ const { Header, Content } = LayoutAntd
 const Layout = (props: any) => {
   const { user, onLogin, onLogout } = props
 
-  const navigate = useNavigate();
-
+  const navigate = useNavigate()
 
   return (
-    <LayoutAntd>
-      <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}>
+    <LayoutAntd className='layout'>
+      <Header className='header'>
         <div className='header-content'>
           <Link to='/'>
             <h1 className='header-text-logo'>
@@ -25,34 +24,30 @@ const Layout = (props: any) => {
             </h1>
           </Link>
 
-          <div className='login-form'>
-            {!user ? (
-              <>
-                <Login onSubmit={onLogin} />
-                <Button onClick={() => navigate('/registry')}>Registry</Button>
-              </>
-            ) : (
-              <>
-                <Dropdown
-                  dropdownRender={() => {
-                    return (
-                      <div style={{ float: 'right' }}>
-                        <Button type='primary' danger onClick={onLogout} >
-                          Logout
-                        </Button>
-                      </div>
-                    )
-                  }}
-                >
-                  <Button>vtanh1905@gmail.com</Button>
-                </Dropdown>
-              </>
-            )}
-          </div>
+          {!user ? (
+            <div className='login-form'>
+              <Login onSubmit={onLogin} />
+              <Button onClick={() => navigate('/registry')}>Registry</Button>
+            </div>
+          ) : (
+            <div className='user-info'>
+              <Space wrap>
+                <span>
+                  Welcome <b>{user.email}</b>
+                </span>
+                <Button type='primary' onClick={() => navigate('/share-video')}>
+                  Share Video
+                </Button>
+                <Button type='primary' danger onClick={onLogout}>
+                  Logout
+                </Button>
+              </Space>
+            </div>
+          )}
         </div>
       </Header>
-      <Content className='site-layout' style={{ padding: '0 50px' }}>
-        <div style={{ padding: 24, minHeight: 380 }}>{props.children}</div>
+      <Content>
+        <div className='content'>{props.children}</div>
       </Content>
     </LayoutAntd>
   )
--- client/src/pages/Registry/index.tsx
+++ client/src/pages/Registry/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Card, Form, Input, Button } from 'antd'
+import './style.scss'
 
 const RegistryPage = () => {
   return (
-    <div>
-      <h1>Registry Page</h1>
+    <div className='registry-page'>
+      <Card title='Registry' style={{ width: '20vw' }}>
+        <Form layout='vertical' labelCol={{ span: 8 }}>
+          <Form.Item
+            label='Email'
+            name='email'
+            rules={[{ required: true, type: 'email', message: 'Email is invalid!' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label='Password'
+            name='password'
+            rules={[{ required: true, message: 'Password is invalid!', min: 6, max: 15 }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 9 }}>
+            <Button type='primary' htmlType='submit'>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   )
 }
